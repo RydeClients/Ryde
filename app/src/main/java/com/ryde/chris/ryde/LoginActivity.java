a33d4b0374228a941bd37cbbc2ee20e7916f8a7c
Chris Sun <chris438@gmail.com>
2016-01-22 20:09:56 -0800

    register a user/login that user with Parse

--- app/src/main/java/com/ryde/chris/ryde/LoginActivity.java
+++ app/src/main/java/com/ryde/chris/ryde/LoginActivity.java
@@ -1,31 +1,75 @@
 package com.ryde.chris.ryde;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+public class LoginActivity extends TouchActivity {
+    public static int REGISTER_USER = 1;
+    private EditText usernameTextField;
+    private EditText passwordTextField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Button loginButton = (Button)findViewById(R.id.btnLogin);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent startMainActivity = new Intent(LoginActivity.this, MainActivity.class);
-                LoginActivity.this.startActivity(startMainActivity);
-            }
-        });
-        Button registerButton = (Button)findViewById(R.id.btnReg);
+        usernameTextField = (EditText) findViewById(R.id.username);
+        passwordTextField = (EditText) findViewById(R.id.password);
+        setUpLoginButtonClickListener();
+        Button registerButton = (Button) findViewById(R.id.btnReg);
+        setTouchNClick(registerButton);
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent startRegisterActivity = new Intent(LoginActivity.this, RegisterActivity.class);
-                LoginActivity.this.startActivity(startRegisterActivity);
+                LoginActivity.this.startActivityForResult(startRegisterActivity, REGISTER_USER);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REGISTER_USER && resultCode == RESULT_OK) {
+            finish();
+        }
+    }
+
+    private void setUpLoginButtonClickListener() {
+        Button loginButton = (Button) findViewById(R.id.btnLogin);
+        setTouchNClick(loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName = usernameTextField.getText().toString();
+                String password = passwordTextField.getText().toString();
+                if (userName.length() == 0 || password.length() == 0) {
+                    Utils.showDialog(LoginActivity.this, R.string.err_fields_empty);
+                    return;
+                }
+                final ProgressDialog dia = ProgressDialog.show(LoginActivity.this, null,
+                        getString(R.string.alert_wait));
+                ParseUser.logInInBackground(userName, password, new LogInCallback() {
+                    @Override
+                    public void done(ParseUser parseUser, ParseException e) {
+                        dia.dismiss();
+                        if (parseUser != null) {
+                            Intent startMainActivity = new Intent(LoginActivity.this, MainActivity.class);
+                            LoginActivity.this.startActivity(startMainActivity);
+                        } else {
+                            Utils.showDialog(LoginActivity.this, getString(R.string.err_login) + " "
+                                    + e.getMessage());
+                            e.printStackTrace();
+                        }
+                    }
+                });
             }
         });
     }
--- app/src/main/java/com/ryde/chris/ryde/RegisterActivity.java
+++ app/src/main/java/com/ryde/chris/ryde/RegisterActivity.java
@@ -1,13 +1,65 @@
 package com.ryde.chris.ryde;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+public class RegisterActivity extends TouchActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        Button registerButton = (Button)findViewById(R.id.btnReg);
+        final EditText passwordField = (EditText)findViewById(R.id.pwd);
+        final EditText emailField = (EditText) findViewById(R.id.email);
+        final EditText usernameField = (EditText) findViewById(R.id.username);
+        setTouchNClick(registerButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String password = passwordField.getText().toString();
+                String email = emailField.getText().toString();
+                String username = usernameField.getText().toString();
+                if (email.length() == 0 || password.length() == 0 || username.length() == 0) {
+                    Utils.showDialog(RegisterActivity.this, R.string.err_fields_empty);
+                    return;
+                }
+                final ProgressDialog dia = ProgressDialog.show(RegisterActivity.this, null,
+                        getString(R.string.alert_wait));
+
+                final ParseUser pu = new ParseUser();
+                pu.setEmail(email);
+                pu.setPassword(password);
+                pu.setUsername(username);
+                pu.signUpInBackground(new SignUpCallback() {
+
+                    @Override
+                    public void done(ParseException e) {
+                        dia.dismiss();
+                        if (e == null) {
+                            setResult(RESULT_OK);
+                            Intent startMainActivity = new Intent(RegisterActivity.this, MainActivity.class);
+                            RegisterActivity.this.startActivity(startMainActivity);
+                            finish();
+                        } else {
+                            Utils.showDialog(
+                                    RegisterActivity.this,
+                                    getString(R.string.err_signup) + " "
+                                            + e.getMessage());
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
     }
 }
