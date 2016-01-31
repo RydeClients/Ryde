84bf95ba05000f4988e594bd6de34999ba264830
Chris Sun <chris438@gmail.com>
2016-01-31 13:25:41 -0800

    Enforce portrait mode in app

--- app/src/main/java/com/ryde/chris/ryde/ChatApplication.java
+++ app/src/main/java/com/ryde/chris/ryde/ChatApplication.java
@@ -1,6 +1,9 @@
 package com.ryde.chris.ryde;
 
+import android.app.Activity;
 import android.app.Application;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
 
 
 import com.firebase.client.Firebase;
@@ -14,6 +17,41 @@ public class ChatApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Firebase.setAndroidContext(this);
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity,
+                    Bundle savedInstanceState) {
+
+                // new activity created; force its orientation to portrait
+                activity.setRequestedOrientation(
+                        ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+
+            }
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+
+            }
+            @Override
+            public void onActivityResumed(Activity activity) {
+            }
+            @Override
+            public void onActivityStopped(Activity activity) {
+
+            }
+            @Override
+            public void onActivityStarted(Activity activity) {
+
+            }
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+            }
+        });
 
     }
 
