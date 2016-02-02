41e5bc8f17b48aaaf6823b75810c81aa536e5ffb
Chris Sun <chris438@gmail.com>
2016-02-02 13:13:20 -0800

    Users can delete existing non groupmarkers

--- app/src/main/java/com/ryde/chris/ryde/MapsActivity.java
+++ app/src/main/java/com/ryde/chris/ryde/MapsActivity.java
@@ -1,16 +1,23 @@
 package com.ryde.chris.ryde;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -41,20 +48,42 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         LatLng startLocation = new LatLng(47.5626337, -122.1344056);
-        mMap.addMarker(new MarkerOptions().position(startLocation).title("Pickup").draggable(true));
+        mMap.addMarker(new MarkerOptions().position(startLocation).title("Pickup").draggable(true).snippet("For Group")
+            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
         moveToCurrentLocation(startLocation);
         LatLng endLocation = new LatLng(47.573733, -122.158436);
-        mMap.addMarker(new MarkerOptions().position(endLocation).title("Destination").draggable(true));
+        mMap.addMarker(new MarkerOptions().position(endLocation).title("Destination").draggable(true).snippet("For Group")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(LatLng latLng) {
-                mMap.addMarker(new MarkerOptions().position(latLng).draggable(true));
+                mMap.addMarker(new MarkerOptions().position(latLng).draggable(true).title("Pickup").snippet("For Chris"));
             }
         });
 
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             mMap.setMyLocationEnabled(true);
         }
+
+        mMap.setInfoWindowAdapter(new MapsInfoWindowAdapter());
+        mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(final Marker marker) {
+                if(!marker.getSnippet().equals("For Group")) {
+                    new AlertDialog.Builder(MapsActivity.this)
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .setMessage("Are you sure you want to delete this marker?")
+                            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    marker.remove();
+                                }
+                            })
+                            .setNegativeButton("No", null)
+                            .show();
+                }
+            }
+        });
     }
 
     private void moveToCurrentLocation(LatLng currentLocation) {
@@ -63,5 +92,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.animateCamera(CameraUpdateFactory.zoomIn());
         // Zoom out to zoom level 10, animating with a duration of 2 seconds.
         mMap.animateCamera(CameraUpdateFactory.zoomTo(13), 2000, null);
+    }
+
+    private class MapsInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
+
+        @Override
+        public View getInfoContents(final Marker marker) {
+            View infoWindow;
+            if(marker.getSnippet().equals("For Group")) {
+                 infoWindow = getLayoutInflater().inflate(R.layout.custom_group_info_window, null);
+            } else {
+                infoWindow = getLayoutInflater().inflate(R.layout.custom_indiv_info_window, null);
+            }
+            ((TextView)infoWindow.findViewById(R.id.pickupOrDropoff)).setText(marker.getTitle());
+            ((TextView)infoWindow.findViewById(R.id.forWhichUser)).setText(marker.getSnippet());
+            return infoWindow;
+        }
+
+        @Override
+        public View getInfoWindow(Marker marker) {
+            return null;
+        }
     }
 }
