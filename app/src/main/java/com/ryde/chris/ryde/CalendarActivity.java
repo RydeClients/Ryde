b9280cdcdc24a6e0f58839f12f6e405b13135313
Chris Sun <chris438@gmail.com>
2016-02-06 17:22:15 -0800

    Calendar should only show one month at a time

--- app/src/main/java/com/ryde/chris/ryde/CalendarActivity.java
+++ app/src/main/java/com/ryde/chris/ryde/CalendarActivity.java
@@ -1,21 +1,96 @@
 package com.ryde.chris.ryde;
 
+import android.content.Context;
 import
         android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.CalendarView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 
 public class CalendarActivity extends AppCompatActivity {
+    private static final int MAX_CELLS = 42;
 
+    private LinearLayout header;
+    private ImageView btnPrev;
+    private ImageView nextButton;
+    private GridView calGrid;
+    private TextView monthDisplay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
-        CalendarView calendar = (CalendarView) findViewById(R.id.calendarView);
-        calendar.setShowWeekNumber(false);
-        calendar.setFirstDayOfWeek(2);
-        calendar.setUnfocusedMonthDateColor(getResources().getColor(R.color.transparent));
+        header = (LinearLayout) findViewById(R.id.calendar_header);
+        btnPrev = (ImageView) findViewById(R.id.calendar_prev_button);
+        nextButton = (ImageView) findViewById(R.id.calendar_next_button);
+        calGrid = (GridView) findViewById(R.id.calendar_grid);
+        monthDisplay = (TextView)findViewById(R.id.calendar_date_display);
+        updateCalendar();
+    }
+
+    private void updateCalendar() {
+        List<Date> days = new ArrayList<Date>();
+        Calendar calendarData = (Calendar)Calendar.getInstance().clone();
+        int monthBeginningCell = calendarData.get(Calendar.DAY_OF_WEEK) - 1;
+        calendarData.add(Calendar.DAY_OF_MONTH, -monthBeginningCell);
+        while(days.size() < MAX_CELLS) {
+            days.add(calendarData.getTime());
+            calendarData.add(Calendar.DAY_OF_MONTH, 1);
+        }
+        calGrid.setAdapter(new RydeCalendarAdapter(this, R.layout.ryde_calendar_date, days));
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMM yyy");
+        monthDisplay.setText(simpleDateFormat.format(new Date().getTime()));
+    }
+
+    private class RydeCalendarAdapter extends ArrayAdapter<Date> {
+        private List<Date> calData;
+        private Context mContext;
+        private int mResource;
+
+        public RydeCalendarAdapter(Context theContext, int resource, List<Date> dates) {
+            super(theContext, resource, dates);
+            mContext = theContext;
+            mResource = resource;
+            calData = dates;
+        }
+        @Override
+        public int getCount() {
+            return calData.size();
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if(convertView == null) {
+                convertView = ((LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE))
+                        .inflate(mResource, null);
+            }
+            Date today = new Date();
+            Date calDate = getItem(position);
+            TextView date = (TextView)convertView.findViewById(R.id.date);
+            if(today.getMonth() != calDate.getMonth() || today.getYear() != calDate.getYear()) {
+                date.setTextColor(getResources().getColor(R.color.greyed_out));
+            } else if (today.getDate() == calDate.getDate()) {
+                date.setTextColor(getResources().getColor(R.color.today));
+            }
+            date.setText(String.valueOf(calDate.getDate()));
+            return convertView;
+        }
     }
 }
